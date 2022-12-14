f16cc242d462f8cea0d1341136fca81da7b7fff8
Kamalabot <35370462+Kamalabot@users.noreply.github.com>
2022-12-14 17:26:32 +0530

    Update hooks.server.js
    Resolving

--- todo/src/hooks.server.js
+++ todo/src/hooks.server.js
@@ -1,7 +1,7 @@
 import postgres from "postgres";
 
 export const handle = async({event, resolve}) =>{
-	const sql = postgres(`postgres://vercel:JPiomQU2AI-CJf96LJm4pw@free-tier14.aws-us-east-1.cockroachlabs.cloud:26257/todoapp?sslmode=verify-full&options=--cluster%3Dcavern-pika-6213`);
+	const sql = postgres(`postgres://********?sslmode=verify-full&options=--cluster%3Dcavern-pika-6213`);
 	//const sql = postgres('postgres://postgres:1234@172.17.0.2:5432/todoapp');
 	
 	event.locals = {
